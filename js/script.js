cc7c41575b4c45302882f558c42fb27b1b37fae4
Bikash Neupane <bikas.neupane2008@gmail.com>
2024-10-07 12:35:22 +1100

    Website UI Updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+
+function toggleUnits(button) {
+    const units = button.nextElementSibling;
+    if (units.style.display === "none") {
+        units.style.display = "block";
+        button.textContent = "See less";
+    } else {
+        units.style.display = "none";
+        button.textContent = "See more";
+    }
+}
